d9b2c02f4367e43b39c6b6dc79a6f66cefcdbf41
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-06-02 16:49:02 -0400

    [FIX] Fix bff, now is just a connector

--- apps/bff/src/orders/orders.controller.ts
+++ apps/bff/src/orders/orders.controller.ts
@@ -1,13 +1,14 @@
-// apps/bff/src/orders/orders.controller.ts
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('orders') // Ruta: http://localhost:3000/api/orders
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @UseGuards(AuthGuard('jwt')) // El BFF solo se encarga de proteger la entrada
   @Post()
   async crearOrden(@Body() datosOrden: any) {
-    return this.ordersService.crearOrdenOrquestada(datosOrden);
+    return this.ordersService.redireccionarAMsOrders(datosOrden);
   }
 }
--- apps/bff/src/orders/orders.service.ts
+++ apps/bff/src/orders/orders.service.ts
@@ -1,38 +1,23 @@
-// apps/bff/src/orders/orders.service.ts
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  // URLs de los microservicios (en producción vendrán del .env)
-  private readonly inventoryUrl = 'http://localhost:3001/api/inventory';
   private readonly ordersUrl = 'http://localhost:3002/api/orders';
 
   constructor(private readonly httpService: HttpService) {}
 
-  async crearOrdenOrquestada(datosOrden: any) {
+  async redireccionarAMsOrders(datosOrden: any) {
     try {
-      // 1. Preguntar a MS-Inventory si hay stock del producto
-      const { data: tieneStock } = await firstValueFrom(
-        this.httpService.get(`${this.inventoryUrl}/check-stock/${datosOrden.productoId}`)
-      );
-
-      if (!tieneStock) {
-        throw new HttpException('No hay stock disponible para este producto', HttpStatus.BAD_REQUEST);
-      }
-
-      // 2. Si hay stock, mandar a crear la orden en MS-Orders
-      const { data: nuevaOrden } = await firstValueFrom(
+      // Pasamanos directo: Envía todo el body tal cual llegó a ms-orders
+      const { data } = await firstValueFrom(
         this.httpService.post(this.ordersUrl, datosOrden)
       );
-
-      // Retornamos al frontend la orden finalizada de forma limpia
-      return nuevaOrden;
-
+      return data;
     } catch (error) {
       throw new HttpException(
-        error.response?.data?.message || 'Error al procesar la orden en el ecosistema',
+        error.response?.data?.message || 'Error en el servidor de Órdenes',
         error.status || HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
