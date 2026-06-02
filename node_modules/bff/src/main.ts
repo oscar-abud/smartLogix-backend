b421778058252632f6fcb0df441f08a025c0f8e4
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-06-02 14:58:05 -0400

    [FEAT] Implement BFF structure with global prefix, CORS, and validation; add auth, inventory, orders, and shipping modules

--- node_modules/bff/src/main.ts
+++ node_modules/bff/src/main.ts
@@ -1,8 +1,24 @@
+// apps/bff/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 1. Prefijo global para las rutas: http://localhost:3000/api/...
+  app.setGlobalPrefix('api');
+
+  // 2. Habilitar CORS para tu Frontend de React
+  app.enableCors({
+    origin: 'http://localhost:5173', // La URL local de tu React + Vite
+    credentials: true,
+  });
+
+  // 3. Validaciones globales para los DTOs que envíe el Front
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log('BFF Orquestador listen at http://localhost:3000/api');
 }
 bootstrap();
