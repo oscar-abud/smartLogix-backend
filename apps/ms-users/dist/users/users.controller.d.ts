01a657f632226a7a8b0d0f0ec0d40e2d49e34aa0
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-06-06 16:53:39 -0400

    merge branch

--- apps/ms-users/dist/users/users.controller.d.ts
+++ apps/ms-users/dist/users/users.controller.d.ts
@@ -1,9 +1,12 @@
 import { UsersService } from './users.service';
 import { ValidateUserDto } from './dto/validate-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 export declare class UsersController {
     private readonly usersService;
     constructor(usersService: UsersService);
+    findAll(): Promise<string | import("./entities/user.entity").User[]>;
+    findUser(id: string): Promise<string | import("./entities/user.entity").User>;
     validateUser(validateUserDto: ValidateUserDto): Promise<{
         id: string;
         email: string;
@@ -16,5 +19,10 @@ export declare class UsersController {
         id_role: number;
         rol: string;
         createdAt: Date;
+    }>;
+    updateUser(id: string, updateUserDto: UpdateUserDto): Promise<import("./entities/user.entity").User & UpdateUserDto>;
+    deleteUser(id: string): Promise<{
+        message: string;
+        id: string;
     }>;
 }
