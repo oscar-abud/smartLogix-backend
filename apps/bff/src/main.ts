e45bc202d4410ab80c6c2e502f4c83769347d517
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-06-07 17:15:49 -0400

    [FIX] Fix cors

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -4,6 +4,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // El puerto exacto donde corre tu React
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Permite el envío de tokens y cookies si fuese necesario
+  });
   app.setGlobalPrefix('api');
 
   // Swagger lee de forma nativa tus controladores manuales del BFF
